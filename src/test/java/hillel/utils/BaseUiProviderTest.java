a41a7d9090e768719582f0178f4df75c45eaaea3
Nina Kotsar <kotsarnina@gmail.com>
2023-12-22 02:20:06 +0200

    jenkins test12

--- src/test/java/hillel/utils/BaseUiProviderTest.java
+++ src/test/java/hillel/utils/BaseUiProviderTest.java
@@ -24,7 +24,7 @@ public class BaseUiProviderTest {
 
     @BeforeMethod
     public void setup() throws MalformedURLException {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromiumdriver().setup();
         ChromeOptions options = new ChromeOptions();
 //        Map<String, Object> prefs = new HashMap<>();
 //        prefs.put("download.default_directory", new File(DOWNLOAD_FOLDER_PATH).getAbsolutePath());
